e54bb1e62f9d4aa08da108da98bc6861994e9ed5
seosigoto <74290267+seosigoto@users.noreply.github.com>
2021-08-06 05:13:37 +0900

    wallet -7

--- Dex.sol
+++ Dex.sol
@@ -1,7 +1,5 @@
 pragma solidity 0.6.3;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
-
 contract Dex {
     struct Token {
         bytes32 ticker;
@@ -25,41 +23,7 @@ contract Dex {
         tokens[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
-    
-    function deposit(
-        uint amount,
-        bytes32 ticker)
-        tokenExist(ticker)
-        external {
-        IERC20(tokens[ticker].tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-        traderBalances[msg.sender][ticker] += amount;
-    }
-    
-    function withdraw(
-        uint amount,
-        bytes32 ticker)
-        tokenExist(ticker)
-        external {
-        require(
-            traderBalances[msg.sender][ticker] >= amount,
-            'balance too low'
-        ); 
-        traderBalances[msg.sender][ticker] -= amount;
-        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
-    }
-    
-    modifier tokenExist(bytes32 ticker) {
-        require(
-            tokens[ticker].tokenAddress != address(0),
-            'this token does not exist'
-        );
-        _;
-    }
-    
+
     modifier onlyAdmin() {
         require(msg.sender == admin, 'only admin');
         _;
